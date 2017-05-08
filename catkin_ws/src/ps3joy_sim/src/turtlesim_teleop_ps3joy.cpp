c4f74c4ce7e57d4844e93d27120dcf8cdaf7fa56
tsun-CPR <tsun@clearpathrobotics.com>
2017-05-08 10:20:34 -0400

    More fine control and steady movement from roomba

--- catkin_ws/src/ps3joy_sim/src/turtlesim_teleop_ps3joy.cpp
+++ catkin_ws/src/ps3joy_sim/src/turtlesim_teleop_ps3joy.cpp
@@ -8,19 +8,24 @@ public:
   TeleopJoyPS3();
   ~TeleopJoyPS3();
   void joyCB(const sensor_msgs::JoyConstPtr& joy);
-  ros::NodeHandle nh_;
-  //ros::NodeHandle nh_pub_;
+  void vel_publish();
+  ros::NodeHandle sh_;
+  ros::NodeHandle ph_;
+  ros::Timer timer_;
 
 private:
   int linear_, angular_;
-  double linear_scale, angular_scale_;
+  double linear_scale_, angular_scale_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
+  geometry_msgs::Twist to_pub;
 };
 
-TeleopJoyPS3::TeleopJoyPS3() : linear_(1), angular_(2) {
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
-  joy_sub_ = nh_.subscribe("/joy",1, &TeleopJoyPS3::joyCB, this);
+TeleopJoyPS3::TeleopJoyPS3() : linear_(1), angular_(2), linear_scale_(0.3), angular_scale_(2.0) {
+    vel_pub_ = ph_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1, true);
+    joy_sub_ = sh_.subscribe("/joy",1, &TeleopJoyPS3::joyCB, this);
+
+    timer_ = sh_.createTimer(ros::Duration(0.1), boost::bind(&TeleopJoyPS3::vel_publish, this));
 }
 
 TeleopJoyPS3::~TeleopJoyPS3() {
@@ -29,15 +34,19 @@ TeleopJoyPS3::~TeleopJoyPS3() {
 
 void TeleopJoyPS3::joyCB(const sensor_msgs::Joy::ConstPtr& joy) {
   geometry_msgs::Twist twistmsg;
-  twistmsg.linear.x = joy->axes[linear_];
-  twistmsg.angular.z = joy->axes[angular_];
-  ros::Rate loop_rate(60);
-  vel_pub_.publish(twistmsg);
+  twistmsg.linear.x = linear_scale_ * joy->axes[linear_];
+  twistmsg.angular.z = angular_scale_ * joy->axes[angular_];
+
+  to_pub = twistmsg;
 }
+
+void TeleopJoyPS3::vel_publish() {
+    vel_pub_.publish(to_pub);
+}
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "joy_teleop_sub");
   //ros::init(argc, argv, "joy_teleop_pub");
   TeleopJoyPS3 teleopJoy;
-
   ros::spin();
 }
